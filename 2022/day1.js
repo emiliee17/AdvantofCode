7285db3be49302e325468a6578bab1ed5b0fd27d
Emilie <emilieenfroy@gmail.com>
2022-12-02 07:49:51 +0100

    update

--- 2022/day1.js
+++ 2022/day1.js
@@ -3,21 +3,25 @@ import * as fs from 'fs';
 const input = fs.readFileSync("input.txt",'utf-8').split('\n\n');
 const input1= input.map((x)=>x.split('\n').map(Number));
 const calories =input1.map((x)=>x.reduce((acc,curr)=> acc+curr));
-const calories_sort=calories.sort(compare);
-const top_three_calories=calories_sort.slice(0,3);
-const sum= top_three_calories.reduce((acc,curr)=>acc+curr);
+//Part_One:
 
+function compare(){
+    let max=calories[0];
+    calories.forEach((x)=> {if(x>max) {max=x;}})
+    return max;
+}
+console.log(compare());
 
-console.log(sum);
-console.log(input.length);
+//Part_Two:
 
-function compare (a,b){
-    if(a>b){
-        return -1;
-
-    }else {
-        return 1;
-    }
+function compare2(){
+    let max=[0,0,0];
+    calories.forEach((x)=> {if(x>max[0]) { max[2]= max[1] ; max[1] = max[0] ;max[0]=x } else{if(x>max[1]){ max[2]=max[1]; max[1]=x} else {if(x>max[2]){max[2]=x}}}} )
+    return max;
 }
 
+//console.log(compare2());
+const sum= compare2().reduce((acc,curr)=>acc+curr);
+
+console.log(sum);
 
