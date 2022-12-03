4d9bdfa202777984265637bc5c227535dc618461
Emilie <emilieenfroy@gmail.com>
2022-12-03 19:18:49 +0100

    update day3

--- 2022/day3.js
+++ 2022/day3.js
@@ -17,35 +17,22 @@ for (let w=0; w<input.length;w++){
             if (first_compartement[w][i]===second_compartment[w][j]){
                 result[w]=first_compartement[w][i];
             }
-            
-    
         }
-        
     }
 }
-
-
-
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const Uppercase = alpha.map((x) => String.fromCharCode(x));
 const Lowercase= Uppercase.map(x=>x.toLowerCase());
 
 const concat=Lowercase.concat(Uppercase);
 
-
-
-
 function final(){
     const result1= result.map((x)=>concat.findIndex(element=> element===x)+1);
     return result1.reduce((acc,curr)=>acc+curr);
 }
-
-console.log('Réponse un:'+final());
+console.log('Réponse 1:'+final());
 
 // Part_Two: 
-
-
-
 
 function triple(){
     for (let i=0;i<(input.length)/3;i++){
@@ -55,26 +42,26 @@ function triple(){
 }
 
 
-
 let result2 =[];
+
 for (let i=0;i<(input.length)/3;i++){
     let triple1= input.slice(i*3,i*3+3);
-    console.log(triple1[0][1]);
     let taille1 =triple1.map((x)=> (x.length));
     let u=0;
-        for(let j=0;j<taille1[0];j++){
-            for (let r=0;r<taille1[1];r++){
-                for(let y=0;y<taille1[2];y++){
-                    if (triple1[0][j]===triple1[1][r] && (triple1[1][r] === triple1[2][y])&&u===0){
-                        result2.push(triple1[0][j]);
-                        u=1;
-                    }
-                }
 
+    let j=0;
+    while(j<taille1[0] && u===0){
+        if (triple1[1].includes(triple1[0][j])){
+            if (triple1[2].includes(triple1[0][j])){
+                result2.push(triple1[0][j]);
+                u=1;
             }
-        }
+        }   
+        j++;            
     }
-console.log(result2);
+    
+}
+   
 
 function final2(){
     const result1= result2.map((x)=>concat.findIndex(element=> element===x)+1);
